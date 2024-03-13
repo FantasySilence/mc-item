58249d046751859c61e96aee220db2216d0de08c
FantasySilence <1735793009@qq.com>
2024-03-13 23:10:56 +0800

    删除不必要的代码段

--- main.py
+++ main.py
@@ -1,12 +1,3 @@
-import os
-import sys
-
-# 全局路径别名
-current_dir = os.path.dirname(os.path.abspath(__file__))
-src_path = os.path.join(current_dir, 'src')
-sys.path.append(src_path)
-
-
 from src.ui.mainWindow import MainWindow
 
 # 主程序入口
