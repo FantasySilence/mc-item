141d413bcd6488f815d65e78d7d187f79a5d2ec1
FantasySilence <1735793009@qq.com>
2024-03-13 23:41:53 +0800

    添加头文件信息

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
+"""
+-----------------------------------------
+@Author: Fantasy_Silence
+@Time: 2024-03-13
+@IDE: Visual Studio Code  Python: 3.9.7
+-----------------------------------------
+"""
+
 from src.ui.mainWindow import MainWindow
 
 # 主程序入口
